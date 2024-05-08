ce3fbd6b1dffe5202ffbc4efc58abd7dc7a06820
Mohamed Boudrar <boudrarmed2003@gmail.com>
2024-05-08 16:50:54 +0100

    sec commit

---
+++ src/components/components/footer.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import "../../index.css";
+
+function Footer() {
+  return (
+    <>
+    <div className="flex flex-col items-center py-10 bg-black">
+    <div className="self-stretch mt-8 w-full bg-white border border-white border-solid min-h-[1px] max-md:max-w-full" />
+
+      <div className="mt-7 text-2xl font-bold text-white">
+        Copyright 2024 ©<br />
+      </div>
+      </div>
+    </>
+  )
+}
+export default Footer;
---
+++ src/components/components/navbar.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import "../../index.css"
+import logo from "../../public/logo.png"
+
+function Nav() {
+  return (
+    <>
+    <div className="flex flex-col items-center py-10 bg-black ">
+      <div className="flex gap-5 px-5 w-full text-white max-w-[1150px] max-md:flex-wrap max-md:max-w-full">
+      <div className="shrink-0 max-w-full aspect-square w-[100px] h-[25px] max-md:mt-10 -mt-7  mr-[200px]">
+    <img src={logo} alt="Logo" />
+</div>
+
+        <div className="flex gap-5 self-start mt-1 text-2xl text-center max-md:flex-wrap max-md:max-w-full">
+        <a href="/" className="underline text-current">Acceuil</a>
+        <a href="/creer-sondage" className="underline text-current">Creer Sondage</a>
+        <a href="/les-sondages" className="underline text-current">Les Sondages</a>
+        <a href="/contact" className="underline text-current">Contact</a>
+
+
+        </div>
+      </div>
+    </div>
+    <div className="self-stretch mt-8 w-full bg-white border border-white border-solid min-h-[1px] max-md:max-w-full" />
+    </>
+  )
+}
+export default Nav;
